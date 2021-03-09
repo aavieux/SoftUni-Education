4f13be392c8987cee247ec1f60a3c70c71eacc06
aavieux <58111856+aavieux@users.noreply.github.com>
2021-03-09 10:01:22 +0200

    09.03.2021

---
+++ 01.Programming-Basics-with-C#-март-2021/02.2 First-Steps-In-Coding-Exercise/Homework/06.Charity Campaign/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06.Charity_Campaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int cookers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            int cakePrice = 45;
+            double wafflePrice = 5.80;
+            double pancakesPrice = 3.20;
+
+            double sum = Math.Round
+                (days * cookers * 
+                ((cakes * cakePrice) + 
+                (waffles * wafflePrice) + 
+                (pancakes * pancakesPrice)),2);
+
+            double finalSum = sum - (sum * 1.00 / 8.00);
+
+            Console.WriteLine(Math.Round(finalSum,2));
+
+
+        }
+    }
+}
---
+++ 01.Programming-Basics-with-C#-март-2021/02.2 First-Steps-In-Coding-Exercise/Homework/07.Fruit Market/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.Fruit_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
