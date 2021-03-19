0c5bf49326245fd5ad6bb719cbe06045c7e7d34f
aavieux <58111856+aavieux@users.noreply.github.com>
2021-03-20 00:59:36 +0200

    20.03.2021

---
+++ 01.Programming-Basics-with-C#-март-2021/03. Loops/Homework/Homework/01.Excellent Result/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01.Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input = double.Parse(Console.ReadLine());
+            if (input >=5.50 && input<=6.00)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
---
+++ 01.Programming-Basics-with-C#-март-2021/03. Loops/Homework/Homework/06.Area of Figures/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06.Area_of_Figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputedFigure = Console.ReadLine();
+            switch (inputedFigure)
+            {
+                case "square":
+                    { 
+                        
+                    }
+                    break;
+                case "rectangle":
+                    { 
+                        
+                    }
+                    break;
+            }
+        }
+    }
+}
